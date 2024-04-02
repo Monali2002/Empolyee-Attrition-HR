4a0f1d8470681974cc36a3400bf2505c1afc8590
Mitali-2002 <mitalipawar20@gmail.com>
2024-04-02 11:00:32 +0530

    Mitali-Commit

--- app/src/main/java/com/example/insightcheck_hr/activity/LoginPage.kt
+++ app/src/main/java/com/example/insightcheck_hr/activity/LoginPage.kt
@@ -1,12 +1,144 @@
 package com.example.insightcheck_hr.activity
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.insightcheck_hr.R
+import com.example.insightcheck_hr.databinding.ActivityLoginPageBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.auth
 
-class LoginPage : AppCompatActivity() {
+class LoginPage : BaseActivity() {
+    private var binding: ActivityLoginPageBinding? = null
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var googleSignInClient: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_page)
+        binding = ActivityLoginPageBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        auth = Firebase.auth
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail().build()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+        binding?.tvRegister?.setOnClickListener {
+            startActivity(Intent(this, SignUpPage::class.java))
+            finish()
+        }
+
+        binding?.tvForgotPassword?.setOnClickListener {
+            startActivity(Intent(this, ForgetPassword::class.java))
+        }
+
+        binding?.btnSignIn?.setOnClickListener {
+            signInUser()
+        }
+
+        binding?.btnSignInWithGoogle?.setOnClickListener {
+            sinInWithGoogle()
+        }
+    }
+
+    private fun signInUser() {
+        val email = binding?.etSinInEmail?.text.toString()
+        val password = binding?.etSinInPassword?.text.toString()
+        if (validateForm(email, password)) {
+            showProgressBar()
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        startActivity(Intent(this, NavBar::class.java))
+                        finish()
+                        hideProgressBar()
+                    } else {
+                        showToast(this, "Can't login successfull")
+                        hideProgressBar()
+                    }
+
+                }
+        }
+    }
+
+    private fun sinInWithGoogle()
+    {
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+    {result->
+        if (result.resultCode == Activity.RESULT_OK)
+        {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResults(task)
+        }
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful)
+        {
+            val account: GoogleSignInAccount? = task.result
+            if (account!=null) {
+                updateUI(account)
+            }
+        }
+        else
+        {
+            Toast.makeText(this,"Sign In Failed, try again.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        showProgressBar()
+        val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+        auth.signInWithCredential(credential).addOnCompleteListener{
+            if (it.isSuccessful)
+            {
+                startActivity(Intent(this, NavBar::class.java))
+                finish()
+                hideProgressBar()
+            }
+            else
+            {
+                showToast(this, "Can't login successfull")
+                hideProgressBar()
+            }
+        }
+    }
+
+    private fun validateForm(email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(email) && !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> {
+                binding?.tilEmail?.error = "Enter valid email address"
+                false
+            }
+
+            TextUtils.isEmpty(password) -> {
+                binding?.tilPassword?.error = "Enter password"
+                binding?.tilEmail?.error = null
+                false
+            }
+
+            else -> {
+                binding?.tilEmail?.error = null
+                binding?.tilPassword?.error = null
+                true
+            }
+        }
     }
 }
--- app/src/main/java/com/example/insightcheck_hr/activity/SignUpPage.kt
+++ app/src/main/java/com/example/insightcheck_hr/activity/SignUpPage.kt
@@ -1,12 +1,78 @@
 package com.example.insightcheck_hr.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import com.example.insightcheck_hr.R
+import com.example.insightcheck_hr.databinding.ActivitySignUpPageBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-class SignUpPage : AppCompatActivity() {
+class SignUpPage : BaseActivity() {
+    private var binding:ActivitySignUpPageBinding? = null
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up_page)
+        binding = ActivitySignUpPageBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        auth = Firebase.auth
+
+        binding?.tvLoginPage?.setOnClickListener {
+            startActivity(Intent(this,LoginPage::class.java))
+            finish()
+        }
+
+        binding?.btnSignUp?.setOnClickListener { registerUser() }
+    }
+
+    private fun registerUser()
+    {
+        val name = binding?.etSinUpName?.text.toString()
+        val email = binding?.etSinUpEmail?.text.toString()
+        val password = binding?.etSinUpPassword?.text.toString()
+        if (validateForm(name, email, password))
+        {
+            showProgressBar()
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){task ->
+                    if (task.isSuccessful)
+                    {
+                        showToast(this,"User id Created Successful")
+                        hideProgressBar()
+                        startActivity(Intent(this,NavBar::class.java))
+                        finish()
+                    }
+                    else {
+                        showToast(this, "User id not Created")
+                        hideProgressBar()
+                    }
+                }
+        }
+    }
+
+    private fun validateForm(name:String, email:String,password:String):Boolean {
+        return when {
+            TextUtils.isEmpty(name) -> {
+                binding?.tilName?.error = "Enter name"
+                false
+            }
+
+            TextUtils.isEmpty(email) && !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> {
+                binding?.tilEmail?.error = "Enter valid email address"
+                false
+            }
+
+            TextUtils.isEmpty(password) -> {
+                binding?.tilPassword?.error = "Enter password"
+                false
+            }
+
+            else -> {
+                true
+            }
+        }
     }
 }
