7906dd57f0621488f6f23ba281dc9138b6deff27
Monali <pawarmonali39@gmail.com>
2024-04-05 14:45:59 +0530

    monali-commit

--- app/src/main/java/com/example/insightcheck_hr/fragments/Home.kt
+++ app/src/main/java/com/example/insightcheck_hr/fragments/Home.kt
@@ -20,6 +20,8 @@ import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
 import java.util.Calendar
 import java.util.Date
 import java.util.Locale
@@ -31,7 +33,14 @@ class Home : Fragment(), CalendarAdapter.onItemClickListener {
     private lateinit var ivCalendarNext: ImageView
     private lateinit var ivCalendarPrevious: ImageView
     private lateinit var barChart: BarChart
+    lateinit var barDataSet1: BarDataSet
+    lateinit var barDataSet2: BarDataSet
 
+    // on below line we are creating array list for bar data
+    lateinit var barEntriesList: ArrayList<BarEntry>
+
+    // creating a string array for displaying days.
+    var branch = arrayOf("Sales", "Accounting", "HR", "Technical", "Support", "IT","product_mng","Marketing","R&D")
 
     private val sdf = java.text.SimpleDateFormat("MMMM yyyy", Locale.ENGLISH)
     private val cal = Calendar.getInstance(Locale.ENGLISH)
@@ -46,32 +55,86 @@ class Home : Fragment(), CalendarAdapter.onItemClickListener {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         barChart = view.findViewById(R.id.bar_chart)
-        val entries = ArrayList<BarEntry>()
-        entries.add(BarEntry(2500f, 0f, "Sales"))
-        entries.add(BarEntry(2000f, 1f, "Accounting"))
-        entries.add(BarEntry(1500f, 2f, "HR"))
-        entries.add(BarEntry(1800f, 3f, "Marketing"))
+        // on below line we are creating a new bar data set
+        barDataSet1 = BarDataSet(getBarChartDataForSet1(), "Present")
+        barDataSet1.setColor(resources.getColor(R.color.Purple40))
+        barDataSet2 = BarDataSet(getBarChartDataForSet2(), "Left")
+        barDataSet2.setColor(resources.getColor(R.color.purple80))
 
-        val barDataSet = BarDataSet(entries, "Employee Count by Department")
-        barDataSet.setColors(
-            Color.parseColor("#FFC107"), // Sales - Orange
-            Color.parseColor("#F44336"), // Accounting - Red
-            Color.parseColor("#2ECC71"), // HR - Green
-            Color.parseColor("#3498DB")  // Marketing - Blue
-        )
-        barDataSet.setValueTextSize(16f)
-//        barDataSet.barWidth = 0.6f// Adjust data label size (optional)
+        // on below line we are adding bar data set to bar data
+        var data = BarData(barDataSet1, barDataSet2)
 
-        val barData = BarData(barDataSet)
+        // on below line we are setting data to our chart
+        barChart.data = data
 
-        barChart.data = barData
-        barChart.description.isEnabled = false // Hide chart description
-        barChart.animateY(1500) // Animate bar entry appearance
-        barChart.xAxis.position = XAxis.XAxisPosition.BOTTOM // Set x-axis position
+        // on below line we are setting description enabled.
+        barChart.description.isEnabled = false
+
+        // on below line setting x axis
+        var xAxis = barChart.xAxis
+
+
+        // below line is to set value formatter to our x-axis and
+        // we are adding our days to our x axis.
+        xAxis.valueFormatter = IndexAxisValueFormatter(branch)
+
+        // below line is to set center axis
+        // labels to our bar chart.
+        xAxis.setCenterAxisLabels(true)
+
+        // below line is to set position
+        // to our x-axis to bottom.
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
+
+        // below line is to set granularity
+        // to our x axis labels.
+        xAxis.setGranularity(0.2f)
+
+        // below line is to enable
+        // granularity to our x axis.
+        xAxis.setGranularityEnabled(true)
+
+        // below line is to make our
+        // bar chart as draggable.
+        barChart.setDragEnabled(true)
+
+        // below line is to make visible
+        // range for our bar chart.
+        barChart.setVisibleXRangeMaximum(3f)
+
+        // below line is to add bar
+        // space to our chart.
+        val barSpace = 0.1f
+
+        // below line is use to add group
+        // spacing to our bar chart.
+        val groupSpace = 0.3f
+
+        // we are setting width of
+        // bar in below line.
+        data.barWidth = 0.15f
+
+        // below line is to set minimum
+        // axis to our chart.
+        barChart.xAxis.axisMinimum = 0f
+
+        // below line is to
+        // animate our chart.
+        barChart.animateY(1500)
+
+        // below line is to group bars
+        // and add spacing to it.
+        barChart.groupBars(0f, groupSpace, barSpace)
 
         // Remove grid lines
-        barChart.axisLeft.setDrawGridLines(false) // Disable left y-axis grid lines
+        barChart.axisLeft.setDrawGridLines(false)
         barChart.xAxis.setDrawGridLines(false)
+        barChart.axisRight.setDrawGridLines(false)
+        barChart.axisRight.isEnabled = false
+
+        // below line is to invalidate
+        // our bar chart.
+        barChart.invalidate()
 
         // Initialize views
         tvDateMonth = view.findViewById(R.id.text_date_month)
@@ -143,5 +206,39 @@ class Home : Fragment(), CalendarAdapter.onItemClickListener {
         calendarList2.addAll(calendarList)
         adapter.setOnItemClickListener(this)
         adapter.setData(calendarList)
+    }
+
+    private fun getBarChartDataForSet1(): ArrayList<BarEntry> {
+        barEntriesList = ArrayList()
+        // on below line we are adding
+        // data to our bar entries list
+        barEntriesList.add(BarEntry(1f, 2689f))
+        barEntriesList.add(BarEntry(2f, 512f))
+        barEntriesList.add(BarEntry(3f, 488f))
+        barEntriesList.add(BarEntry(4f, 1854f))
+        barEntriesList.add(BarEntry(5f, 1509f))
+        barEntriesList.add(BarEntry(6f, 384f))
+        barEntriesList.add(BarEntry(7f, 818f))
+        barEntriesList.add(BarEntry(8f, 576f))
+        barEntriesList.add(BarEntry(9f, 561f))
+        barEntriesList.add(BarEntry(10f, 609f))
+        return barEntriesList
+    }
+
+    private fun getBarChartDataForSet2(): ArrayList<BarEntry> {
+        barEntriesList = ArrayList()
+        // on below line we are adding data
+        // to our bar entries list
+        barEntriesList.add(BarEntry(1f, 550f))
+        barEntriesList.add(BarEntry(2f, 109f))
+        barEntriesList.add(BarEntry(3f, 113f))
+        barEntriesList.add(BarEntry(4f, 390f))
+        barEntriesList.add(BarEntry(5f, 312f))
+        barEntriesList.add(BarEntry(6f, 52f))
+        barEntriesList.add(BarEntry(7f, 158f))
+        barEntriesList.add(BarEntry(8f, 110f))
+        barEntriesList.add(BarEntry(9f, 112f))
+        barEntriesList.add(BarEntry(10f, 85f))
+        return barEntriesList
     }
 }
